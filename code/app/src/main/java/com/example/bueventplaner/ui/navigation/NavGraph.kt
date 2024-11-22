7268f6e9659c9949f3883fd113097607daf8ae29
Yuchen Cao <caoyc@bu.edu>
2024-11-21 22:30:07 -0500

    Update

--- code/app/src/main/java/com/example/bueventplaner/ui/navigation/NavGraph.kt
+++ code/app/src/main/java/com/example/bueventplaner/ui/navigation/NavGraph.kt
@@ -6,14 +6,14 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.bueventplaner.ui.pages.LoginPage
 import com.example.bueventplaner.ui.pages.SignupPage
-import com.example.bueventplaner.ui.pages.OnboardingPage
+//import com.example.bueventplaner.ui.pages.OnboardingPage
 
 @Composable
 fun NavGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "login") {
         composable("login") { LoginPage(navController) }
         composable("signup") { SignupPage(navController) }
-        composable("onboarding") { OnboardingPage(navController) }
+//        composable("onboarding") { OnboardingPage(navController) }
         composable("home") { /* TODO: Home Page Implementation */ }
     }
 }
